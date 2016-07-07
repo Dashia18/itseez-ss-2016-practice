e7fc7259c3f0a3dd2b3864acf2b58ba526680779
Dashia18 <das-bobkova@yandex.ru>
2016-07-07 17:52:46 +0300

    tracing.cpp not finished, stop at opticalFlow

--- src/tracking.cpp
+++ src/tracking.cpp
@@ -1,6 +1,7 @@
 #include "tracking.hpp"
 
 #include <iostream>
+#include <vector>
 
 using std::string;
 using std::shared_ptr;
@@ -29,21 +30,49 @@ bool MedianFlowTracker::Init(const cv::Mat &frame, const cv::Rect &roi) {
 cv::Rect MedianFlowTracker::Track(const cv::Mat &frame) {
 
 	//Features
-	std::vector< cv::Point2f > corners;
+	std::vector< cv::Point2f > cornersOld;
 	int maxCorners = 10;
 	double qualityLevel = 0.01;
 	double minDistance = 20.;
 	
-	Mat grey;
-	Mat greyConvert;
-	cvtColor(frame, grey, CV_BGR2GRAY);
-	grey.convertTo(greyConvert, CV_8UC1);
-	cv::goodFeaturesToTrack(greyConvert(position_), corners, maxCorners, qualityLevel, minDistance);
+	Mat greyOld;
+	Mat greyOldConvert;
+	cvtColor(frame_, greyOld, CV_BGR2GRAY);
+	greyOld.convertTo(greyOldConvert, CV_8UC1);
+	cv::goodFeaturesToTrack(greyOldConvert(position_), cornersOld, maxCorners, qualityLevel, minDistance);
+
+	//recalc coordinade + position_ (x, y) - roi area
+	for (int ind = 0; ind < cornersOld.size(); ind++) {
+		cornersOld[ind].x = cornersOld[ind].x + position_.x;
+		cornersOld[ind].y = cornersOld[ind].y + position_.y;
+	}
 
 	//OpticalFlow
-	calcOpticalFlowPyrLK
+	Mat greyNew;
+	Mat greyNewConvert;
+	cv::Mat frameCopy;
+	frame.copyTo(frameCopy);
+	cvtColor(frameCopy, greyNew, CV_BGR2GRAY);
+	greyNew.convertTo(greyNewConvert, CV_8UC1);
 	
+	if (!cornersOld.empty())
+	{
+		std::vector< cv::Point2f > cornersNew;
+		std::vector <uchar> status;
+		std::vector <float> err;
+		cv::calcOpticalFlowPyrLK(greyOldConvert, greyNewConvert, cornersOld, cornersNew, status, err);
 
+		int indWithOutBad = 0;
+		for (int indCornerNew = 0; indCornerNew < cornersNew.size(); indCornerNew++)
+		{	
+			if (!status[indCornerNew] == 0) {
+				cornersNew[indWithOutBad++] = cornersNew[indCornerNew];                                      
+			}
+		}
+		cornersNew.resize(indWithOutBad);
+	}
+	
+	//median shift tbd
 
 
 
