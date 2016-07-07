d28e440967ac48aeae5f69a52c0816207e1c2703
Dashia18 <das-bobkova@yandex.ru>
2016-07-07 16:38:53 +0300

    tracing.cpp in work

--- src/tracking.cpp
+++ src/tracking.cpp
@@ -9,5 +9,47 @@ using namespace cv;
 shared_ptr<Tracker> Tracker::CreateTracker(const string &name) {
   std::cerr << "Failed to create tracker with name '" << name << "'"
             << std::endl;
+  if (name == "median_flow") {
+	  return std::make_shared<MedianFlowTracker>();
+  }
   return nullptr;
+}
+
+bool MedianFlowTracker::Init(const cv::Mat &frame, const cv::Rect &roi) {
+	if (!frame.empty()) {
+		frame.copyTo(frame_);
+		position_ = Rect(roi.x, roi.y, roi.width, roi.height);
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+cv::Rect MedianFlowTracker::Track(const cv::Mat &frame) {
+
+	//Features
+	std::vector< cv::Point2f > corners;
+	int maxCorners = 10;
+	double qualityLevel = 0.01;
+	double minDistance = 20.;
+	
+	Mat grey;
+	Mat greyConvert;
+	cvtColor(frame, grey, CV_BGR2GRAY);
+	grey.convertTo(greyConvert, CV_8UC1);
+	cv::goodFeaturesToTrack(greyConvert(position_), corners, maxCorners, qualityLevel, minDistance);
+
+	//OpticalFlow
+	calcOpticalFlowPyrLK
+	
+
+
+
+
+
+
+	Rect rectTemp;
+	return rectTemp;
+
 }
